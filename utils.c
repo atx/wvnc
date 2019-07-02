073c7c822dc735c3bcd33eee295ddb47ce13e3ce
Josef Gajdusek <atx@atx.name>
2019-07-02 21:19:24 +0200

    utils: Retab

--- utils.c
+++ utils.c
@@ -11,58 +11,62 @@
 
 void fail(const char *format, ...)
 {
-    va_list vas;
-    va_start(vas, format);
-    vfprintf(stderr, format, vas);
-    va_end(vas);
-    fprintf(stderr, "\n");
-    exit(EXIT_FAILURE);
+	va_list vas;
+	va_start(vas, format);
+	vfprintf(stderr, format, vas);
+	va_end(vas);
+	fprintf(stderr, "\n");
+	exit(EXIT_FAILURE);
 }
 
 
 static void log_base(const char *prefix, const char *format, va_list vas)
 {
-    fputs(prefix, stderr);
-    fputc(' ', stderr);
-    vfprintf(stderr, format, vas);
-    fputc('\n', stderr);
+	fputs(prefix, stderr);
+	fputc(' ', stderr);
+	vfprintf(stderr, format, vas);
+	if (format[strlen(format) - 1] != '\n') {
+	// This is for libvncserver
+	fputc('\n', stderr);
+	}
+	fputs("\x1b[0m", stderr);
 }
 
 
 void log_info(const char *format, ...)
 {
-    va_list vas;
-    va_start(vas, format);
-    log_base("[I]", format, vas);
-    va_end(vas);
+	va_list vas;
+	va_start(vas, format);
+	log_base("\x1b[1m\x1b[94m[I]", format, vas);
+	va_end(vas);
 }
 
 
 void log_error(const char *format, ...)
 {
-    va_list vas;
-    va_start(vas, format);
-    log_base("[E]", format, vas);
-    va_end(vas);
+	va_list vas;
+	va_start(vas, format);
+	log_base("\x1b[1m\x1b[91m[E]", format, vas);
+	va_end(vas);
 }
 
 
 void *xmalloc(size_t size)
 {
-    void *ptr = malloc(size);
-    if (ptr == NULL) {
-        fail("Memory allocation failed");
-    }
-    memset(ptr, 0, size);
-    return ptr;
+	void *ptr = malloc(size);
+	if (ptr == NULL) {
+		fail("Memory allocation failed");
+	}
+	memset(ptr, 0, size);
+	return ptr;
 }
 
 
 uint64_t time_monotonic()
 {
-    struct timespec time;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &time);
-    return time.tv_sec * 1000000 + time.tv_nsec / 1000;
+	struct timespec time;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+	return time.tv_sec * 1000000 + time.tv_nsec / 1000;
 }
 
 
